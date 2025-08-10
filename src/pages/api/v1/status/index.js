6ca090f637b5f96fdfb5e6dfe755997b7d32344a
alexdlli <alexsandrojr.303@gmail.com>
2025-08-09 21:26:08 -0300

    fix: makes `src/infra/database.js` `client.connect()` more robust to errors

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,6 +1,6 @@
 import database from "@/infra/database";
 
-async function status(request, response) {
+async function status(_request, response) {
   const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
